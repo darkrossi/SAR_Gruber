ae0885e5078bb15a55734dee5a5911a6aea21534
darkrossi <mickael.fournier@phelma.grenoble-inp.fr>
2016-01-16 17:47:50 +0100

    Changement du delay d'envoi de messages en broadcast (mis en random)

--- Boedec.Fournier/src/messages/engine/BroadcastThread.java
+++ Boedec.Fournier/src/messages/engine/BroadcastThread.java
@@ -7,6 +7,7 @@ package messages.engine;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  *
@@ -16,30 +17,32 @@ public class BroadcastThread extends Thread {
 
     private Peer m_peer;
     private NioEngine m_engine;
+    boolean m_running = true;
+    static int m_port_listening;
 
     public BroadcastThread(Peer m_peer, NioEngine m_engine) {
         this.m_peer = m_peer;
         this.m_engine = m_engine;
+        m_port_listening = m_engine.m_port_listening;
     }
 
     @Override
     public void run() {
         super.run();
 
-        for (;;) {
+        while (m_running) {
 
-        	/*
+            /*
         	 * On crée un tableau de data propre au Peer, qui sera broadcasté au groupe.
-        	 */
+             */
             int length = 3;
             byte bytes[] = new byte[length];
             for (int i = 0; i < length; i++) {
-                bytes[i] = (byte) (i + 8 * (m_engine.m_port_listening - 2004));
+                bytes[i] = (byte) (i + 8 * (m_port_listening - 2004));
             }
-            
+
             /**
-             * On ajoute en tête du message le type de message
-             *  0 = DATA, 1 = ACK
+             * On ajoute en tête du message le type de message 0 = DATA, 1 = ACK
              */
             byte finalBytes[] = new byte[bytes.length + 1];
             finalBytes[0] = 0;
@@ -56,11 +59,18 @@ public class BroadcastThread extends Thread {
 
             m_engine.getM_selector().wakeup();
 
+            int random_delay = randInt(1, 6000);
             try {
-                sleep(2000); // 2 secondes d'attente. Facilite les tests, mais devra être retiré lors du test en burst mode.
+                this.sleep(random_delay); // 2 secondes d'attente. Facilite les tests, mais devra être retiré lors du test en burst mode.
             } catch (InterruptedException ex) {
                 interrupt();
             }
         }
+    }
+
+    public static int randInt(int min, int max) {
+        Random rand = new Random(m_port_listening);
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+        return randomNum;
     }
 }
